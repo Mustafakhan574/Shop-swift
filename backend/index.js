61426052bee223630ea6364796be57d83f787c16
Mustafakhan574 <mk3855315@gmail.com>
2026-01-04 11:23:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ let app = express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-     origin:["http://localhost:5173","http://localhost:5174"],
+     origin:["https://shop-swift-frontend786.onrender.com","http://localhost:5174"],
      credentials:true
 }))
 app.use("/api/auth",authRoutes)
