4b40bce4fbb947f71aa91993bbf4a9229a7e05a2
Mustafa khan <mk3855315@gmail.com>
2026-01-04 10:46:48 +0530

    Fix Vite config for frontend root and build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  root: './frontend',
   plugins: [react(),tailwindcss(),],
-  
+  build: {
+    outDir: 'dist', // output folder inside frontend
+    rollupOptions: {
+      input: 'src/main.jsx' // entry point
+    }
+  },
+  resolve: {
+    alias: {
+      // optional: make sure imports inside frontend work correctly
+      '@': '/src'
+    }}
 })
